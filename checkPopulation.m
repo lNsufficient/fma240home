772457df3a63b30d95967544b487a939843f5ebf
lNsufficient <edvard.jo@gmail.com>
2016-03-10 11:29:03 +0100

    started on u6

---
+++ checkPopulation.m
@@ -0,0 +1,6 @@
+function isValid = checkPopulation(x, a, k)
+%ISOK Checks if x is okay for the knapsack problem
+
+isValid = (x*a <= k);
+end
+
---
+++ u6.m
@@ -0,0 +1,44 @@
+clear;
+c = [10 5 2]';
+a = [1 1 1]';
+k = 1.2;
+
+if (length(a) ~= length(c)) 
+    error('invalid a, c')
+end
+if (k < min(a))
+    error('invalid a, k')
+end
+if (min(a) <= 0)
+    error('invalid a, zero weight')
+end
+if (min(c) <= 0)
+    disp('not knapsack problem?')
+end
+
+%Generate population:
+N = 30;
+meanSum = sum(a)/2;
+nfactor = min(meanSum/k, 1);
+Ncorr = 2;
+Ntry = nfactor*N*Ncorr;
+chromLength = length(a);
+population = randi(2, round(Ntry), chromLength) - 1;
+validVector = checkPopulation(population, a, k);
+keptPop = population(validVector, :);
+[nbrChrom, ~] = size(keptPop);
+while(nbrChrom < N*0.8)
+    tempN = (N - nbrChrom)*nfactor;
+    tempPopulation = randi(2, round(tempN), chromLength) - 1;
+    tempValid = checkPopulation(tempPopulation, a, k);
+    tempKept = tempPopulation(tempValid, :);
+    [nbrNew, ~] = size(tempKept);
+    keptPop = [keptPop; tempKept];
+    nbrChrom = nbrNew + nbrChrom;
+    Ncorr = Ncorr*1.1
+end
+if (nbrChrom > N*1.2)
+    keptPop = keptPop(1:round(N*1.2), :);
+end
+size(keptPop)
+checkPopulation(keptPop, a, k)'
