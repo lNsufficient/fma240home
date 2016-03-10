b18258414deff9d8b2f657ecd80a15ba2b3e77db
lNsufficient <edvard.jo@gmail.com>
2016-03-10 13:56:28 +0100

    u6 working

--- knapSack.m
+++ knapSack.m
@@ -5,3 +5,16 @@ k = 3;
 maxRounds = 10;
 
 [optval optimal_chromosones Nfactor] = geneticKnapsack(c, a, k, maxRounds)
+%%
+%friluftsliv:
+clear;
+clc;
+
+maxRounds = 10;
+
+a = [1, 3, 0.5, 10, 0.1, 1, 1.5, 0.3, 0.2, 2]';
+c = [10 10 6 12 8 1 3 7 8 10]';
+k = 12;
+
+[optval optimal_chromosones Nfactor] = geneticKnapsack(c, a, k, maxRounds)
+optimal_chromosone*c
